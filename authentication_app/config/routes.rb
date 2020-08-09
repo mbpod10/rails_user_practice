4b9c00ed9c94644538a376ed53a40725ec4ba651
M. Brock Podgurski <mbpod10@gmail.com>
2020-08-09 12:57:40 -0400

    users routes work

--- authentication_app/config/routes.rb
+++ authentication_app/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
+  resources :users, only: [:index, :show]
 end
 
 
